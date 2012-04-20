4ddc83e5790e6b1240d3374eb9198e43c1af98e2
U-Obsidion\Alexis Shaw <Alexis Shaw@Obsidion.(none)>
2012-04-20 21:33:47 +1000

    First commit

---
+++ parseline.py
@@ -0,0 +1,18 @@
+import re
+
+def tabsToSpaces(line):
+    noSpacesSoFar = 0
+    noSpacesToAdd = 0
+    for c in line:
+        if c == ' ':
+            noSpacesSoFar += 1
+        elif c == '\t':
+            noSpacesToAdd += 8 - noSpacesSoFar%8
+            noSpacesSoFar += 8 - noSpacesSoFar%8
+        else:
+            break
+    spaces = ''.join(['' for i in xrange(noSpacesToAdd)])
+    re.sub("\t","")
+    return spaces + line
+
+
