06eafbe5352e9525145801b6413b4204a3a2f786
Alexis Shaw <alexis.shaw@gmail.com>
2012-04-21 23:37:20 +1000

    (23:30-23:38) Updated tester to loop over arguments

--- testScript.sh
+++ testScript.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-./python2perl "$1" > out.pl
-chmod +x out.pl
-$1 > py.out && ./out.pl > pl.out && diff py.out pl.out && rm py.out pl.out out.pl
+for f in $@
+do
+    ./python2perl "$f" > out.pl
+    chmod +x out.pl
+    if $1 > py.out && ./out.pl > pl.out && diff py.out pl.out && rm py.out pl.out out.pl
+    then
+       echo "$f passed"
+    else
+       echo "$f failed"
+    fi
+done
