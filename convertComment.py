ca91b87e810bf6ef04b7ffd450d2008bdb2c6945
Alexis Shaw <alexis.shaw@gmail.com>
2012-04-22 23:54:02 +1000

    (23:41-23:48) Bugfixes

--- convertComment.py
+++ convertComment.py
@@ -4,7 +4,7 @@ import tokenize
 __author__ = 'Alexis Shaw'
 
 def convertComment(token,line,t,v,i,understood,variables):
-    if i == 0 and re.match(r'^#!',v): line += '#!/usr/bin/perl -w'
+    if i == 0 and re.match(r'^#!',v): line += '#!/usr/bin/perl -w\n'
     elif token[i + 1][0] == tokenize.NEWLINE : line += ';' + v
     else: line += v
     return line, i, understood, variables
