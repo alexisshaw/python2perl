6d58c10bb53ffc93ef1ad86de4fad0a9301a6b24
Alexis Shaw <alexis.shaw@gmail.com>
2012-04-23 00:15:39 +1000

    (23:57-00:12) Bugfixes

--- convertInt.py
+++ convertInt.py
@@ -7,5 +7,5 @@ def convertInt(token, line, t,v,i,understood, variables):
      if token[i+1][0] == tokenize.OP   and token[i+1][1] == '(':
          toConvert= ''
          toConvert, i, understood,variables = convertGrouping(token,toConvert,t,v,i+1,understood,variables)
-         line += '(sprintf ("%d", ' + toConvert + ') + 1 - 1)'
+         line += '(sprintf ("%d", ' +'scalar'+ toConvert + ') + 1 - 1)'
      return line,i,understood,variables
