4bab4f3c0b410f402b9523dad76090d9368b6efa
Alexis Shaw <alexis.shaw@gmail.com>
2012-04-23 01:20:16 +1000

    (00:12-01:15) Fixed up indentation

--- convertGrouping.py
+++ convertGrouping.py
@@ -10,15 +10,15 @@ def convertGrouping(token,line,t,v,i,understood,variables):
     while len(token)-i > 0 :
         (t, v, _, _,_) = token[i]
         if t == tokenize.NAME:
-            line,i,understood,variables = convertToken.convertToken(token, line,t,v,i,understood,variables)
+            line,i,understood,variables = convertToken.convertToken(token, line,t,v,i,understood,variables,'')
         elif t == tokenize.OP and re.match(r'^[(<>&^|~=+*%-]$|^\*\*$|<<|>>|>=|<=|!=|==',v):
-            line,i,understood,variables = convertToken.convertToken(token, line,t,v,i,understood,variables)
+            line,i,understood,variables = convertToken.convertToken(token, line,t,v,i,understood,variables,'')
         elif t == tokenize.NL or t == tokenize.NUMBER:
-            line,i,understood,variables = convertToken.convertToken(token,line,t,v,i,understood,variables)
+            line,i,understood,variables = convertToken.convertToken(token,line,t,v,i,understood,variables,'')
         elif t == tokenize.STRING:
-            line,i,understood,variables = convertToken.convertToken(token,line,t,v,i,understood,variables)
+            line,i,understood,variables = convertToken.convertToken(token,line,t,v,i,understood,variables,'')
         elif t == tokenize.COMMENT and token[i+1][0] == tokenize.NL:
-            line,i,understood,variables = convertToken.convertToken(token,line,t,v,i,understood,variables)
+            line,i,understood,variables = convertToken.convertToken(token,line,t,v,i,understood,variables,'')
         elif t == tokenize.OP and re.match('[)]', v):
             line += v + ' '
             break
--- convertNot.py
+++ convertNot.py
@@ -14,16 +14,16 @@ def convertNot(token,line,t,v,i,understood,variables):
     while len(token)-i > 0 :
         (t, v, _, _,_) = token[i]
         if t == tokenize.NAME and not keyword.iskeyword(v):
-            line,i,understood,variables = convertToken.convertToken(token, line,t,v,i,understood,variables)
+            line,i,understood,variables = convertToken.convertToken(token, line,t,v,i,understood,variables,'')
         elif t == tokenize.OP and re.match(r'^[()<>&^|/~=+*%,-]$|^\*\*$|<<|>>|>=|<=|!=|==|\+=|-=|\*=|%=|&=',v):
-            line,i,understood,variables = convertToken.convertToken(token, line,t,v,i,understood,variables)
+            line,i,understood,variables = convertToken.convertToken(token, line,t,v,i,understood,variables,'')
         elif t == tokenize.NL or t == tokenize.NUMBER:
-            line,i,understood,variables = convertToken.convertToken(token,line,t,v,i,understood,variables)
+            line,i,understood,variables = convertToken.convertToken(token,line,t,v,i,understood,variables,'')
         elif t == tokenize.STRING:
-            line,i,understood,variables = convertToken.convertToken(token,line,t,v,i,understood,variables)
+            line,i,understood,variables = convertToken.convertToken(token,line,t,v,i,understood,variables,'')
         elif t == tokenize.COMMENT and token[i+1][0] == tokenize.NL:
-            line,i,understood,variables = convertToken.convertToken(token,line,t,v,i,understood,variables)
+            line,i,understood,variables = convertToken.convertToken(token,line,t,v,i,understood,variables,'')
         else:
             line += ') '
-            return convertToken.convertToken(token,line,t,v,i,understood,variables)
+            return convertToken.convertToken(token,line,t,v,i,understood,variables,'')
         i += 1
